5a711f09388ba2cb8c6ae8517f332a845226fe37
ankitx55 <ankitx55@gmail.com>
2022-08-09 17:04:18 +0530

    new changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const Product = require('./models/product');
 
-mongoose.connect("mongodb://localhost:27017/farmStand")
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("mongo connection open");
   })
@@ -16,14 +16,20 @@ mongoose.connect("mongodb://localhost:27017/farmStand")
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}))
 
+const categories = ['fruit', 'vegetable', 'dairy'];
 // app.get('/dogs', ( req, res) => {
 //   res.send("working fine on dogs");
 //   console.log("on dogs");
 // });
 
 
-app.get('/product', async(req, res) => {
+
+
+
+
+app.get('/products', async(req, res) => {
   const products = await Product.find({})
   // console.log(products)
   // res.send('all products will be here')
@@ -32,6 +38,46 @@ app.get('/product', async(req, res) => {
 
   // res.render('products/index.ejs')
   // console.log('new files ')
+})
+
+
+
+
+
+
+
+
+
+app.get('/products/new', (req,res) => {
+  res.render('products/new')
+})
+
+app.post('/products', async (req, res)=>{
+  // console.log(req.body)
+  const newProduct = new Product(req.body)
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/products/:id',async(req,res)=>{
+  const {id} = req.params;
+  const product = await Product.findById(id)
+  res.render('products/show',{product})
+  // console.log(product)
+  // res.send('details page')
 })
 
 app.listen(3000, () => {
