2b17432922d77f3b641efd04d31bf8c9cd60a12b
ankitx55 <ankitx55@gmail.com>
2022-08-21 11:52:30 +0530

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-
+const methodOverride = require('method-override')
 const Product = require('./models/product');
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,7 +16,13 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended:true}))
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'))
+
+
+
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 // app.get('/dogs', ( req, res) => {
@@ -79,6 +85,24 @@ app.get('/products/:id',async(req,res)=>{
   // console.log(product)
   // res.send('details page')
 })
+
+
+/*Edit page*/ 
+app.get('/products/:id/edit', async(req,res) =>{
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', {product})
+})
+
+app.put('/products/:id', async(req,res)=>{
+  const {id} = req.params;
+  const  product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new:true})
+  res.redirect(`/products/${product._id}`);
+  console.log(req.body);
+})
+
+
+
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
